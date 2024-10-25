eadc146fedb97f0e308ca10e7b284800855ccf3f
Pieter Viljoen <ptr727@users.noreply.github.com>
2024-10-25 21:09:44 +0000

    Ssync with upstream

--- examples/monitor_all.py
+++ examples/monitor_all.py
@@ -18,35 +18,16 @@ parser.add_argument("--debug", help="enable debug logging", action="store_true")
 args = parser.parse_args()
 
 
-def parse_arguments() -> argparse.Namespace:
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description="aiovantage example")
-    parser.add_argument("host", help="hostname of Vantage controller")
-    parser.add_argument("--username", help="username for Vantage controller")
-    parser.add_argument("--password", help="password for Vantage controller")
-    parser.add_argument(
-        "--debug", help="enable debug level logging", action="store_true"
-    )
-    parser.add_argument("--log", help="write debug log to file")
-    return parser.parse_args()
+def callback(event: VantageEvent, obj: SystemObject, data: dict[str, Any]) -> None:
+    """Print out any state changes."""
+    object_type = type(obj).__name__
 
-
-def configure_logging(log_args: argparse.Namespace) -> None:
-    """Configure debug logging."""
-    logging.basicConfig(
-        level=logging.DEBUG if log_args.debug else logging.INFO,
-        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] %(message)s",
-        handlers=[
-            logging.StreamHandler(),
-        ],
-    )
-    if log_args.log:
-        logging.getLogger().addHandler(logging.FileHandler(filename=args.log, mode="w"))
-
-
-# Parse command line args and configure logging
-args = parse_arguments()
-configure_logging(args)
+    if event == VantageEvent.OBJECT_ADDED:
+        print(f"[{object_type} added] '{obj.name}' ({obj.id})")
+    elif event == VantageEvent.OBJECT_UPDATED:
+        print(f"[{object_type} updated] '{obj.name}' ({obj.id})")
+        for attr in data.get("attrs_changed", []):
+            print(f"    {attr} = {getattr(obj, attr)}")
 
 
 async def main() -> None:
@@ -67,4 +48,4 @@ async def main() -> None:
 
 
 with contextlib.suppress(KeyboardInterrupt):
-    asyncio.run(main(), debug=args.debug)
+    asyncio.run(main())
